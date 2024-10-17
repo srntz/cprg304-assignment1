11c6adf37abe75f4fc28c27f545cd8793232cd0d
s-raniytuz <tuzsocial909@gmail.com>
2024-10-17 14:19:52 -0600

    Implement quicksort for Comparable[]

--- src/utilities/Sort.java
+++ src/utilities/Sort.java
@@ -1,6 +1,7 @@
 package utilities;
 
 import java.util.Comparator;
+import java.util.Random;
 
 import shapes.Shape;
 
@@ -39,17 +40,54 @@ public class Sort {
 	}
 	
 	public static void quicksort(Comparable[] array) {
-		//TODO later
+		quicksort(array, 0, array.length - 1);
 	}
 	
 	public static <T> void quicksort(T[] array, Comparator<? super T> c) {
 		//TODO later
 	}
+
+	private static void quicksort(Comparable[] array, int lowIndex, int highIndex) {
+		if(lowIndex >= highIndex) {
+			return;
+		}
+
+		int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+		Comparable pivot = array[pivotIndex];
+		swap(array, pivotIndex, highIndex);
+
+		int lp = lowIndex;
+		int hp = highIndex;
+
+		while (lp < hp) {
+			while (array[lp].compareTo(pivot) != 1 && lp < hp) {
+				lp++;
+			}
+
+			while (array[hp].compareTo(pivot) != -1 && lp < hp) {
+				hp--;
+			}
+
+			swap(array, lp, hp);
+		}
+
+		swap(array, lp, highIndex);
+		quicksort(array, lowIndex, lp - 1);
+		quicksort(array, lp + 1, highIndex);
+
+	}
 	
+	private static <T> void swap(T[] array, int i, int j) {
+		T temp = array[j];
+		array[j] = array[i];
+		array[i] = temp;
+	}
+
 	public static void mysort(Comparable[] array) {
 		//TODO later
 	}
-	
+
+
 	public static <T> void mysort(T[] array, Comparator<? super T> c) {
 		//TODO later
 	}
